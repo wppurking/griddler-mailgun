1cdabb8f2292d4e0118d8cca44e0c30a6ceaf90d
wyatt <wppurking@gmail.com>
2017-04-07 09:24:17 +0800

    * 因为 Griddler 可以自定义 EmailClass 后, 取消对 EmailClass 的 open class 改造

--- lib/griddler/mailgun.rb
+++ lib/griddler/mailgun.rb
@@ -2,16 +2,4 @@ require 'griddler'
 require 'griddler/mailgun/version'
 require 'griddler/mailgun/adapter'
 
-module Griddler
-  module Mailgun
-  end
-
-  # open class for some function
-  class Email
-    def content_ids
-      @params[:content_ids].presence || []
-    end
-  end
-end
-
 Griddler.adapter_registry.register(:mailgun, Griddler::Mailgun::Adapter)
